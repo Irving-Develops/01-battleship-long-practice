79cea9bc37d4ebccfcedd069c989a4af730eab0b
Irving Arreola Palacios <irving.a789@gmail.com>
2022-05-11 16:02:22 -0500

    board complete

--- 01-battleship-dom/assets/js/index.js
+++ 01-battleship-dom/assets/js/index.js
@@ -6,17 +6,21 @@ let board = new Board(); // creates a new game board
 // Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 
+
 const boardArea = document.createElement("div");
 boardArea.id = "board";
 document.body.appendChild(boardArea);
-boardArea.append(board);
-
 // access the cell to make them squares
-// for (let i = 0; i < board.grid.length; i++) {
-//     for (let j = 0; j < board.grid[0].length; j++)  {
-//         board.grid[i].style.height = "30px";
+let alphaVal = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+for (let i = 0; i < board.grid.length; i++) {
+    for (let j = 0; j < board.grid[0].length; j++)  {
+        let letter = alphaVal[j];
+       let cell =  document.createElement("div")
+        boardArea.appendChild(cell)
+        cell.id = `${letter}-${i+1}`;
 
-//     }
-// }
+        
+    }
+}
 
 // Your code here
