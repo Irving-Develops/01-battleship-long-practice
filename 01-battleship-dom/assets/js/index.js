6ca49200f5b277b857d05dce520807e75227a602
Krista Strucke <kstrucke@gmail.com>
2022-05-11 13:25:58 -0700

    grids

--- 01-battleship-dom/assets/js/index.js
+++ 01-battleship-dom/assets/js/index.js
@@ -6,4 +6,17 @@ let board = new Board(); // creates a new game board
 // Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 
+const boardArea = document.createElement("div");
+boardArea.id = "board";
+document.body.appendChild(boardArea);
+boardArea.append(board);
+
+// access the cell to make them squares
+// for (let i = 0; i < board.grid.length; i++) {
+//     for (let j = 0; j < board.grid[0].length; j++)  {
+//         board.grid[i].style.height = "30px";
+
+//     }
+// }
+
 // Your code here
